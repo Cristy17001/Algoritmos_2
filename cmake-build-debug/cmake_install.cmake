63d4cfc06ab222c56c53ef9064860e1e5ab66bf4
Tomás <tomas.eiras03@gmail.com>
2022-12-29 11:25:46 +0000

    Menu Layouts

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Cristiano/Desktop/OneDrive - Universidade do Porto/2 Ano/Algoritmos e Estruturas de Dados/Trabalho_Algoritmos_2
+# Install script for directory: F:/ProjetoVoos/Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/tomas/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/223.8214.51/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Cristiano/Desktop/OneDrive - Universidade do Porto/2 Ano/Algoritmos e Estruturas de Dados/Trabalho_Algoritmos_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/ProjetoVoos/Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -3,12 +3,75 @@
 
 using namespace std;
 
+void airportInfoMenu(){
+    string choice;
+    cout
+            << '\n'
+            << "###############################################################################\n"
+            << "#                           Airport Information Menu                          #\n"
+            << "###############################################################################\n"
+            << "# Please choose an option:                                                    #\n"
+            << "# 1. Number of departures                                                     #\n"
+            << "# 2. How many different companies did the airport serve as a source for       #\n"
+            << "# 3. How many different destinations                                          #\n"
+            << "# 4. How many different countries                                             #\n"
+            << "# 5. How many locations are reachable using a maximum of Y flights            #\n"
+            << "# Q. Back                                                                     #\n"
+            << "###############################################################################\n"
+            << "#> ";
+
+    cin >> choice;
+    if (choice.size() > 1) {choice.clear(); choice[0] = '?';}
+    choice = (char)toupper(choice[0]);
+
+    switch (choice[0]) {
+        case '1': break;
+        case '2': break;
+        case '3': break;
+        case '4': break;
+        case '5': break;
+        case 'Q': break;
+        case '?': {cout << endl << "Error: Invalid input. Please enter one character." << endl << endl; break;}
+        default : {cout << endl << "Error: Invalid input. Please enter a valid choice." << endl << endl; break;}
+    }
+}
+
+void flightOptionsMenu(){
+    string choice;
+    cout
+        << '\n'
+        << "###############################################################################\n"
+        << "#                           Flight Options Menu                               #\n"
+        << "###############################################################################\n"
+        << "# Specify the type of location you want to input:                             #\n"
+        << "# 1. Airport                                                                  #\n"
+        << "# 2. City and Country                                                         #\n"
+        << "# 3. Latitude and Longitude                                                   #\n"
+        << "# Q. Back                                                                     #\n"
+        << "###############################################################################\n"
+        << "#> ";
+
+    cin >> choice;
+    if (choice.size() > 1) {choice.clear(); choice[0] = '?';}
+    choice = (char)toupper(choice[0]);
+
+    switch (choice[0]) {
+        case '1': break;
+        case '2': break;
+        case '3': break;
+        case 'Q': break;
+        case '?': {cout << endl << "Error: Invalid input. Please enter one character." << endl << endl; break;}
+        default : {cout << endl << "Error: Invalid input. Please enter a valid choice." << endl << endl; break;}
+    }
+}
+
 int main() {
     Manager manager = Manager();
     string choice;
 
     do {
         cout
+        <<'\n'
         << "###############################################################################\n"
         << "#                              Flight Manager                                 #\n"
         << "###############################################################################\n"
@@ -23,14 +86,20 @@ int main() {
         choice = (char)toupper(choice[0]);
 
         switch (choice[0]) {
-            case '1': break;
-            case '2': break;
+            case '1':
+                flightOptionsMenu();
+                break;
+
+            case '2':
+                airportInfoMenu();
+                break;
+
             case 'Q': break;
             case '?': {cout << endl << "Error: Invalid input. Please enter one character." << endl << endl; break;}
             default : {cout << endl << "Error: Invalid input. Please enter a valid choice." << endl << endl; break;}
         }
 
-    } while (choice[0] != 'q');
+    } while (choice[0] != 'Q');
 
     return 0;
 }
