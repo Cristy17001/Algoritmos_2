3eddb7d19013c9f63485ac0b9e785ba8d3e221e7
Tomás <tomas.eiras03@gmail.com>
2023-01-08 18:02:09 +0000

    é isto

--- FlightGraph.h
+++ FlightGraph.h
@@ -10,17 +10,17 @@
 
 using namespace std;
 
-/**
- * \struct Node
- * \brief Nodes of the FlightGraph.
- *
- * Designed for the capability of storing data of every flight, including
- * \param code - the airport of origin
- * \param neighbors - unordered_map capable of storing the destination airport and the possible airlines to achieve it, and insertion of them in O(1) time complexity
- * \param n_visited - auxiliary counter for the design of the BFS
- *
- * \see FlightGraph
- */
+    /**
+    * @struct Node
+    * @brief Nodes of the FlightGraph.
+    *
+    * Designed for the capability of storing data of every flight, including
+    * @param code - the airport of origin
+    * @param neighbors - unordered_map capable of storing the destination airport and the possible airlines to achieve it, and insertion of them in O(1) time complexity
+    * @param n_visited - auxiliary counter for the design of the BFS
+    *
+    * @see FlightGraph
+    */
 struct Node {
     string code;
     // First field is the destination and the vector are the airlines from which it can travel
@@ -29,16 +29,16 @@ struct Node {
 };
 
 
-/**
- * \class FlightGraph
- * \brief Graph storing every flight possibility.
- *
- * Designed for node access in O(1) time complexity, by using unordered_map<string, Node*> for node access.
- * Simple directed graph with implementations of BFS to find shortest path between two airports.
- *
- * \note This class was designed to be used by the manager class.
- * \see Manager
- */
+    /**
+    * @class FlightGraph
+    * @brief Graph storing every flight possibility.
+    *
+    * Designed for node access in O(1) time complexity, by using unordered_map<string, Node*> for node access.
+    * Simple directed graph with implementations of BFS to find shortest path between two airports.
+    *
+    * @note This class was designed to be used by the manager class.
+    * @see Manager
+    */
 class FlightGraph {
 
 private:
@@ -46,92 +46,94 @@ private:
 
 public:
     FlightGraph();
+
     /**
-     * \brief Adds a new node.
-     *
-     * Creates a new node* if the airport isn't yet in the map ignores otherwise
-     * \param code - airport code
-     *
-     */
+    * @brief Adds a new node.
+    *
+    * Creates a new node* if the airport isn't yet in the map ignores otherwise
+    * @param code - airport code
+    *
+    */
     void AddNode(const string& code);
 
     /**
-      * \brief Adds a link from source node to target node.
-      *
-      * Adds a new destination node to source neighbors. If the link already existed then adds the airline, otherwise creates a new link by adding to neighbors map the target node with the airline
-      *
-      * \param source source airport.
-      * \param target target airport.
-      * \param airline airline from source to target
-      *
-      *
-      */
+    * @brief Adds a link from source node to target node.
+    *
+    * Adds a new destination node to source neighbors. If the link already existed then adds the airline, otherwise creates a new link by adding to neighbors map the target node with the airline
+    *
+    * @param source source airport.
+    * @param target target airport.
+    * @param airline airline from source to target
+    *
+    *
+    */
     void AddEdge(const string& source, const string& target, const string& airline);
     unordered_map<string, Node*> get_nodes() const;
 
     /**
-      * \brief Performs BFS search for the shortest paths.
-      *
-      * Performs a breadth-first search (BFS) on a graph and keeps track of the path taken through the graph during the search.
-      * The function also checks if the target node is found during the search, and if it is found, the path taken to reach the target node is added to a return vector of paths.
-      * This search as a time complexity of O(n + e) where n is the number of nodes and e is the number of edges
-      *
-      * \param source_name source airport.
-      * \param target_name target airport.
-      *
-      * \return Shortest paths from the source airport to the target airport stored in a vector<vector<Node*>>
-      */
+    * @brief Performs BFS search for the shortest paths.
+    *
+    * Performs a breadth-first search (BFS) on a graph and keeps track of the path taken through the graph during the search.
+    * The function also checks if the target node is found during the search, and if it is found, the path taken to reach the target node is added to a return vector of paths.
+    * This search as a time complexity of O(n + e) where n is the number of nodes and e is the number of edges
+    *
+    * @param source_name source airport.
+    * @param target_name target airport.
+    *
+    * @return Shortest paths from the source airport to the target airport stored in a vector<vector<Node*>>
+    */
     vector<vector<Node*>> BfsShortestPaths(const string& source_name, const string& target_name);
 
     /**
-      * \brief Transforms the paths in to a readable format.
-      *
-      * Transforms the vector<vector<Node*>> receive from the BFS into to vector<vector<string>> while also adding every possible airline combination for the specific path.
-      * Therefore having a time complexity of O(n*m) being n the number of paths and m the number of nodes in each path
-      *
-      * \param paths all possible path received from the BfsShortestPaths.
-      *
-      * \return Shortest paths in format [Origin, airline1, stopage1, airline2, Destination] as a vector<vector<string>>.
-      */
+    * @brief Transforms the paths in to a readable format.
+    *
+    * Transforms the vector<vector<Node*>> receive from the BFS into to vector<vector<string>> while also adding every possible airline combination for the specific path.
+    * Therefore having a time complexity of O(n*m) being n the number of paths and m the number of nodes in each path
+    *
+    * @param paths all possible path received from the BfsShortestPaths.
+    *
+    * @return Shortest paths in format [Origin, airline1, stopage1, airline2, Destination] as a vector<vector<string>>.
+    */
     static vector<vector<string>> transformer(const vector<vector<Node*>>& paths);
 
     /**
-      * \brief Performs BFS search with n_flights
-      *
-      * Performs a breadth-first search (BFS) on a graph to find all nodes that can be reached from a given starting node in a maximum of n_flights.
-      * The time complexity of this function is O(n + m), where n is the number of nodes in the graph and m is the number of edges.
-      * \param airport source airport
-      * \param n_flights number of flights
-      *
-      * \return set of airport codes reachable from one airport with n_flights
-      */
+    * @brief Performs BFS search with n_flights
+    *
+    * Performs a breadth-first search (BFS) on a graph to find all nodes that can be reached from a given starting node in a maximum of n_flights.
+    * The time complexity of this function is O(n + m), where n is the number of nodes in the graph and m is the number of edges.
+    * @param airport source airport
+    * @param n_flights number of flights
+    *
+    * @return set of airport codes reachable from one airport with n_flights
+    */
     unordered_set<string> BfsNflights(const string& airport, int n_flights);
 
     /**
-      * The time complexity of this function is O(1)
-      * \param airport - source airport
-      * \return number of neighbors for a given airport
-      */
+    * The time complexity of this function is O(1)
+    * @param airport - source airport
+    * @return number of neighbors for a given airport
+    */
     int n_flights(const string& airport);
+
     /**
-      * The time complexity of this function is O(n), where n is the number of neighbors of the input node
-      * \param airport - source airport
-      * \return number of different airlines that can be used with one flight
-      */
+    * The time complexity of this function is O(n), where n is the number of neighbors of the input node
+    * @param airport - source airport
+    * @return number of different airlines that can be used with one flight
+    */
     int diff_airline(const string& airport);
 
     /**
-      * The time complexity of this function is O(1)
-      * \param airport - source airport
-      * \return number of different destinations from one airport
-      */
+    * The time complexity of this function is O(1)
+    * @param airport - source airport
+    * @return number of different destinations from one airport
+    */
     int n_diff_dest(const string& airport);
 
     /**
-      * The time complexity of this function is O(n), where n is the number of neighbors of the input node
-      * \param airport - source airport
-      * \return vector of different destinations from one airport
-      */
+    * The time complexity of this function is O(n), where n is the number of neighbors of the input node
+    * @param airport - source airport
+    * @return vector of different destinations from one airport
+    */
     vector<string> diff_dest(const string& airport);
 };
 
--- Manager.h
+++ Manager.h
@@ -29,46 +29,65 @@ struct PairEqual {
 // Specify a type for airports map consisting of a pair Country, City
 using CityToAirportsMap = unordered_map<pair<string, string>, vector<string>, PairHash, PairEqual>;
 
-/**
- * \class Manager
- * \brief Stores data about flights, airports and airlines
- *
- * Makes the data accessible everywhere in the program. Makes it possible to access most of data in O(1) complexity.
- * Also responsible for loading of data from .csv files
- *
- * \note This class was designed to store data about flights, airports and airlines.
- * \see FlightGraph, Airport, Airlines
- */
+    /**
+    * @class Manager
+    * @brief Stores data about flights, airports and airlines
+    *
+    * Makes the data accessible everywhere in the program. Makes it possible to access most of data in O(1) complexity.
+    * Also responsible for loading of data from .csv files
+    *
+    * \note This class was designed to store data about flights, airports and airlines.
+    * @see FlightGraph, Airport, Airlines
+    */
 class Manager {
 private:
 
     /**
-     * Provides access to the flights graph
-     * \see FlightGraph
-     */
+    * Provides access to the flights graph
+    * @see FlightGraph
+    */
     FlightGraph flights;
 
     /**
-     * Provides access to every airport in O(1) time complexity
-     * \see Airport
-     */
+    * Provides access to every airport in O(1) time complexity
+    * @see Airport
+    */
     unordered_map<string, Airport> airport;
 
     /**
-     * Provides access to every airline in O(1) time complexity
-     * \see Airlines
-     */
+    * Provides access to every airline in O(1) time complexity
+    * @see Airlines
+    */
     unordered_map<string, Airlines> airlines;
 
     /**
-     * Provides access to every airport by the city and country of that airport in O(1) time complexity
-     * \see Airport
-     */
+    * Provides access to every airport by the city and country of that airport in O(1) time complexity
+    * @see Airport
+    */
     CityToAirportsMap cities_to_airports;
 
 public:
+
+    /**
+    * @brief Constructs a Manager object
+    *
+    * This is the default constructor for the Manager class. It initializes the internal data structures to their default state.
+    */
     Manager();
 
+    /**
+    * @enum InputType
+    * @brief Enumeration for specifying the type of input
+    *
+    * The InputType enum is used to specify the type of input when calling the findShortestPathConditions() method.
+    *
+    * @var Airports
+    * Indicates that the input is an airport code
+    * @var Cities
+    * Indicates that the input is a city name
+    * @var Coordinates
+    * Indicates that the input is a pair of latitude and longitude coordinates
+    */
     enum InputType {
         Airports,
         Cities,
@@ -78,26 +97,101 @@ public:
 
     //void set_Flights() const;
     FlightGraph get_Flights() const;
-
     unordered_map<string, Airport> get_airport() const;
 
-    unordered_map<string, Airlines> get_airlines() const;
 
+    unordered_map<string, Airlines> get_airlines() const;
     CityToAirportsMap get_cities_to_airports() const;
 
-    vector<string> getAirportsByCity (const string& coutry, const string& city) const;
 
+    vector<string> getAirportsByCity (const string& coutry, const string& city) const;
     vector<vector<string>> getMinPathCombinations (const vector<string>& sourceAirports, const vector<string>& targetAirports, vector<string> valid_airlines, bool combination) const;
 
+    /**
+    * @brief Finds the shortest path(s) between two input locations
+    *
+    * This method finds the shortest path(s) between the specified source and target locations, subject to the given constraints. The source and target locations can be specified as airport codes, city names, or latitude and longitude coordinates.
+    *
+    * The complexity of this function is O(n), where n is the number of airports in the graph.
+    *
+    * @param inputType The type of the source location (airport code, city name, or coordinates)
+    * @param destinationType The type of the target location (airport code, city name, or coordinates)
+    * @param country The country in which the source location is located (if inputType is City or Airport)
+    * @param input The source location (airport code, city name, or coordinates)
+    * @param countryD The country in which the target location is located (if destinationType is City or Airport)
+    * @param target The target location (airport code, city name, or coordinates)
+    * @param inputLat The latitude of the source location (if inputType is Coordinates)
+    * @param inputLong The longitude of the source location (if inputType is Coordinates)
+    * @param inputDist The maximum distance from the source location (if inputType is Coordinates)
+    * @param valid_airlines A list of valid airline names
+    * @param combination A flag indicating whether to return all combinations or just the shortest path(s)
+    * @return A list of combinations of flights between the source and target locations
+    */
     vector<vector<string>> findShortestPathConditions(InputType inputType, InputType destinationType,const string &country, const string &input,const string &countryD, const string &target, double inputLat, double inputLong, double inputDist,const vector<string>& valid_airlines, bool combination) const;
 
+    /**
+    * @brief Loads flight data from a CSV file
+    *
+    * This method loads flight data from a CSV file and stores it in the internal FlightGraph object.
+    * The file must be in a specific format as described in the project documentation.
+    * @see FlightGraph
+    * @param filename The path to the CSV file
+    */
     void load_Flights(const std::string& filename);
+
+    /**
+    * @brief Loads airport data from a CSV file
+    *
+    * This method loads airport data from a CSV file and stores it in the internal map of Airport objects.
+    * @see Airport
+    * @param filename The path to the CSV file
+    */
     void load_Airports(const std::string& filename);
+
+    /**
+    * @brief Loads airline data from a CSV file
+    *
+    * This method loads airline data from a CSV file and stores it in the internal map of Airlines objects.
+    * @see Airlines
+    * @param filename The path to the CSV file
+    */
     void load_Airlines(const std::string& filename);
 
+    /**
+    * @brief Calculates the distance between two pairs of latitude and longitude coordinates
+    *
+    * This method calculates the distance between two pairs of latitude and longitude coordinates using the Haversine formula.
+    *
+    * @param lat1 The latitude of the first coordinate
+    * @param lon1 The longitude of the first coordinate
+    * @param lat2 The latitude of the second coordinate
+    * @param lon2 The longitude of the second coordinate
+    * @return The distance between the two coordinates in kilometers
+    */
     static double haversine(double lat1, double lon1, double lat2, double lon2);
-    int diff_countrys(const string& airport);
+
+    /**
+    * @brief Counts the number of different countries that are reachable from a given airport
+    *
+    * This method returns the number of different countries that can be reached from the specified airport, within a certain number of flights.
+    *
+    * @param airp The airport code of the starting airport
+    * @return The number of different countries that can be reached from the starting airport
+    */
+    int diff_countrys(const string& airp);
+
+
+    /**
+    * @brief Counts the number of airports, cities, and countries that are reachable from a given airport
+    *
+    * This method returns the number of airports, cities, and countries that can be reached from the specified airport, within a certain number of flights. The results are returned in a vector with the following format: [num_airports, num_cities, num_countries]
+    *
+    * @param airp The airport code of the starting airport
+    * @param n_flights The maximum number of flights
+    * @return A vector containing the number of airports, cities, and countries that can be reached from the starting airport
+    */
     vector<int> n_flights_bfs(const string& airp, int n_flights);
+
 };
 
 #endif
